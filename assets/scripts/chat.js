5a72726a69e399f6825176b519ebae57daa17dd2
Agniiis <116561851+Agniiis@users.noreply.github.com>
2023-06-10 14:28:59 +0300

    Update chat.js

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -1,4 +1,6 @@
 const username = 'User#' + Math.floor(Math.random() * 1000);
+const vulgarWords = ["lohs", "stulbenis", "idiots"]; // Vulgāro vārdu saraksts
+
 
 document.getElementById("chat-user-greeting").innerHTML = 'Welcome, <b>' + username + '</b>!';
 
@@ -11,9 +13,23 @@ function sendChatMessage() {
 
   const box = document.getElementById("chatbox");
 
+  const filteredText = filterVulgarWords(text);
   const msgln = document.createElement("div");
   msgln.classList.add("msgln");
-  msgln.innerHTML = username + ': ' + text;
+  msgln.innerHTML = username + ': ' + filteredText;
 
   box.appendChild(msgln);
+}
+
+function filterVulgarWords(message) {
+  const words = message.split(" ");
+  const filteredWords = words.map(word => {
+    const lowercasedWord = word.toLowerCase();
+    if (vulgarWords.includes(lowercasedWord)) {
+      return "*".repeat(word.length);
+    }
+    return word;
+  });
+
+  return filteredWords.join(" ");
 }
