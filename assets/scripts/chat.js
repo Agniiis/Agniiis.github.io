2c9fcc8a12b6c5098243ba7edf46563657e8db5b
Agniiis <116561851+Agniiis@users.noreply.github.com>
2023-06-12 05:51:36 +0300

    Update chat.js

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -1,17 +1,27 @@
 const username = 'User#' + Math.floor(Math.random() * 1000);
-const vulgarWords = ["lohs", "stulbenis", "idiots"]; // Vulgāro vārdu saraksts
+const vulgarWords = ["lohs", "stulbenis", "idiots", "bla","ble"]; // Vulgāro vārdu saraksts
 
 
 document.getElementById("chat-user-greeting").innerHTML = 'Welcome, <b>' + username + '</b>!';
 
+let lastMessageTimestamp = 0;
+
 function sendChatMessage() {
   const input = document.getElementById("usermsg");
   const text = input.value;
-  if (!text) return;
+  if (!text) return; //Ja teksts ir tukš, tad atgriežas un nepievieno pap. kodu
 
   input.value = '';
 
   const box = document.getElementById("chatbox");
+
+  const currentTime = Date.now();
+  if (currentTime - lastMessageTimestamp < 1000) { // Pārbauda vai laika starpība starp pašreizējo ziņu un pēdējo ziņa ir 1 sekundes
+    console.log("Jūs varat sūtīt ziņas reizi sekundē!!");
+    return;
+  }
+
+  lastMessageTimestamp = currentTime;
 
   const filteredText = filterVulgarWords(text);
   const msgln = document.createElement("div");
@@ -20,16 +30,16 @@ function sendChatMessage() {
 
   box.appendChild(msgln);
 }
-
+//Necenzēto vārdu funkcija
 function filterVulgarWords(message) {
   const words = message.split(" ");
   const filteredWords = words.map(word => {
     const lowercasedWord = word.toLowerCase();
-    if (vulgarWords.includes(lowercasedWord)) {
+    if (vulgarWords.includes(lowercasedWord)) { //Pārbauda vai vārds ir vulgārs
       return "*".repeat(word.length);
     }
     return word;
   });
 
-  return filteredWords.join(" ");
+  return filteredWords.join(" "); //Apvieno filtrētos vārdus atpakaļ virknē
 }
